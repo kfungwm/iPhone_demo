a4d8a9716e690d078b4045313b2e1cd7373f741a
kfungwm <kevinfung87@gmail.com>
2024-08-12 15:46:47 +0200

    added video slides

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,8 +24,8 @@ const Hero = () => {
   }, [])
 
   useGSAP(() => {
-    gsap.to('#hero', { opacity: 1, delay: 1.5 })
-    gsap.to('#cta', { opacity: 1, delay: 2, y: 0, stragger: 0.3 })
+    gsap.to('#hero', { opacity: 1, delay: 1 })
+    gsap.to('#cta', { opacity: 1, delay: 1.5, y: 0, duration: 1, stagger: 0.3 })
   }, [])
 
   return (
